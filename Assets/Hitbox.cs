ab6375d31ca73950a8a873989bdc2a550a67d122
Forgotten Team <forgottenrealm66@gmail.com>
2026-01-27 20:03:55 -0300

    Sistema de Hitbox

---
+++ Assets/Hitbox.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+    BoxCollider2D col;
+    public int damage = 10;
+    private bool hasHit;
+    
+    public void ResetHit()
+    {
+        hasHit = false;
+    }
+    private void OnEnable()
+    {
+        hasHit = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (hasHit) return;
+
+        if (other.CompareTag("Enemy"))
+        {
+            hasHit = true;
+            Debug.Log("Acertou Mizeravi");
+        }
+    }
+
+    private void Awake()
+    {
+        col = GetComponent<BoxCollider2D>();
+    }
+    public void ReactivateCollider()
+    {
+        col.enabled = false;
+        col.enabled = true;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, GetComponent<BoxCollider2D>().size);
+    }
+}
